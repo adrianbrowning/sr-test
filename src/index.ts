fdd7ae16a5ce0e41c8e518a913f52af398e37c79
Adrian Elton-Browning <adrian.browning@tealium.com>
2024-08-05 19:07:50 +0100

    ISSUE-48
    feat(core): new feature

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ console.log("Testing husky commitlint");
 console.log("New feature!");
 console.log("New feature!");
 console.log("New feature!");
+console.log("New feature!");
 console.info("This is a fix!");
 console.info("This is a fix!");
 console.info("This is a fix!");
